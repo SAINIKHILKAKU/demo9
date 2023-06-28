e5d17cf95cce470f70846743520aa30d6b94e85f
SAINIKHIL KAKU <sainikhil684@gmail.com>
2023-06-28 15:04:18 +0530

    first commit

---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+
+import AuthContext from "../../store/auth-context";
+import classes from "./Navigation.module.css";
+
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
+
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/"></a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/"></a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
